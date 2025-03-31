43ad48613f7c6d246ad599cda3f02399d44df740
kasiye taye <kasiyet@tnt.com>
2025-03-31 13:39:48 +0300

    hfjgjgf

---
+++ ecom/app/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATE_CHOICES=(
+    ('AA','Addis Ababa'),
+    ('AF','Afar'),
+    ('AM','Amhara'),
+    ('BG','Benishangul-Gumuz'),
+    ('DR','Dire Dawa'),
+    ('GB','Gambela'),
+    ('HR','Harari'),
+    ('OR','Oromia'),
+    ('SM','Somali'),
+    ('SE','South Ethiopia'),
+    ('SER','Southwest Ethiopia Peoples Region'),
+    ('TR','Tigray'),
+)
+CATEGORY_CHOICES=(
+    ('CR','Curd'),
+    ('ML','Milk'),
+    ('LS','Lassi'),
+    ('MS','Milkshake'),
+    ('PN','Panner'),
+    ('GH','Ghee'),
+    ('CZ','Cheese'),
+    ('IC','Ice-Creams'),
+)
+
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default="")
+    prodapp = models.TextField(default="")
+    #brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='products/', null=True, blank=True)
+    def _str_(self):
+        return self.title
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)  # Fixed typo
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=15)
+  
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+
+    def __str__(self):  # Fixed method 2name
+        return self.name
+    
+class supply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)  # Fixed typo
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=15)
+  
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+
+    def __str__(self):  # Fixed method 2name
+        return self.name
